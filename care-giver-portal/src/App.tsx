5712fb26dce5f950afa2a343dc187c4911905ed9
Muskan2518 <muskan220405@gmail.com>
2025-05-18 10:21:45 +0530

    first commit

--- care-giver-portal/src/App.tsx
+++ care-giver-portal/src/App.tsx
@@ -8,6 +8,7 @@ import CaregiverDashboard from './pages/CaregiverDashboard';
 import DashboardHome from './pages/DashboardHome';
 import SettingsPage from './pages/SettingsPage';
 import ProtectedRoute from './components/ProtectedRoute';
+import MMSE from './pages/mmsetest';
 
 export default function App() {
   return (
@@ -15,7 +16,7 @@ export default function App() {
       {/* Public Routes */}
       <Route path="login" element={<Login />} />
       <Route path="signup" element={<Signup />} />
-      
+
       {/* Protected Routes */}
       <Route element={<ProtectedRoute />}>
         <Route path="dashboard" element={<CaregiverDashboard />}>
@@ -24,9 +25,14 @@ export default function App() {
           <Route path="reminders" element={<RemindersPage />} />
           <Route path="settings" element={<SettingsPage />} />
         </Route>
+
+        {/* MMSE Test as a separate protected page */}
+        <Route path="mmsetest" element={<MMSE />} />
+
+        {/* Redirect root to dashboard */}
         <Route path="/" element={<Navigate to="/dashboard" replace />} />
       </Route>
-      
+
       {/* Catch all */}
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
---
+++ care-giver-portal/src/pages/mmsetest.tsx
@@ -0,0 +1,7 @@
+export default function MMSE(){
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
